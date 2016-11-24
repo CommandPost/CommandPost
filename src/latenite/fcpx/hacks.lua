6d03104047cf6c83cc740a95089ac9a4f7f0833b
David Peterson <david@randombits.org>
2016-11-24 19:18:42 +1000

    * Migrated code from init.lua into latenite/fcpx/hacks.lua to simplify the global init.lua script.

--- src/latenite/fcpx/hacks.lua
+++ src/latenite/fcpx/hacks.lua
@@ -9,26 +9,26 @@ local osascript 				= require("hs.osascript")
 -------------------------------------------------------------------------------
 -- SCRIPT VERSION:
 -------------------------------------------------------------------------------
-local hacksVersion = "0.70"
+scriptVersion = "0.70"
 -------------------------------------------------------------------------------
 
-mod.version() {
-	return hacksVersion;
-}
+function mod.version()
+	return scriptVersion;
+end
 
 --------------------------------------------------------------------------------
 -- IS FINAL CUT PRO INSTALLED:
 --------------------------------------------------------------------------------
-function isFinalCutProInstalled()
-	return doesDirectoryExist('/Applications/Final Cut Pro.app')
+function mod.isFinalCutProInstalled()
+	return mod.doesDirectoryExist('/Applications/Final Cut Pro.app')
 end
 
 --------------------------------------------------------------------------------
 -- RETURNS FCPX VERSION:
 --------------------------------------------------------------------------------
-function finalCutProVersion()
-	if isFinalCutProInstalled() then
-		ok,appleScriptFinalCutProVersion = hs.osascript.applescript('return version of application "Final Cut Pro"')
+function mod.finalCutProVersion()
+	if mod.isFinalCutProInstalled() then
+		ok,appleScriptFinalCutProVersion = osascript.applescript('return version of application "Final Cut Pro"')
 		return appleScriptFinalCutProVersion
 	else
 		return "Not Installed"
@@ -36,11 +36,82 @@ function finalCutProVersion()
 end
 
 --------------------------------------------------------------------------------
+-- LAUNCH FINAL CUT PRO:
+--------------------------------------------------------------------------------
+function mod.launchFinalCutPro()
+	hs.application.launchOrFocus("Final Cut Pro")
+end
+
+--------------------------------------------------------------------------------
+-- IS FINAL CUT PRO FRONTMOST?
+--------------------------------------------------------------------------------
+function mod.isFinalCutProFrontmost()
+
+	local fcpx = hs.appfinder.appFromName("Final Cut Pro")
+	if fcpx == nil then
+		return false
+	else
+		return fcpx:isFrontmost()
+	end
+
+end
+
+--------------------------------------------------------------------------------
 -- DOES DIRECTORY EXIST:
 --------------------------------------------------------------------------------
-function doesDirectoryExist(path)
-    local attr = hs.fs.attributes(path)
+function mod.doesDirectoryExist(path)
+    local attr = fs.attributes(path)
     return attr and attr.mode == 'directory'
+end
+
+--------------------------------------------------------------------------------
+-- DISPLAY ALERT MESSAGE:
+--------------------------------------------------------------------------------
+function mod.displayAlertMessage(whatMessage)
+	local returnToFinalCutPro = mod.isFinalCutProFrontmost()
+	local appleScriptA = 'set whatMessage to "' .. whatMessage .. '"' .. '\n\n'
+	local appleScriptB = [[
+		tell me to activate
+		display dialog whatMessage buttons {"OK"} with icon stop
+	]]
+	osascript.applescript(appleScriptA .. appleScriptB)
+	if returnToFinalCutPro then mod.launchFinalCutPro() end
+end
+
+--------------------------------------------------------------------------------
+-- INITIALISES THE HACK
+--------------------------------------------------------------------------------
+function mod.init()
+	--------------------------------------------------------------------------------
+	-- CLEAR THE CONSOLE:
+	--------------------------------------------------------------------------------
+	hs.console.clearConsole()
+
+	--------------------------------------------------------------------------------
+	-- DISPLAY WELCOME MESSAGE IN THE CONSOLE:
+	--------------------------------------------------------------------------------
+	print("====================================================")
+	print("                  FCPX Hacks v" .. mod.version()     )
+	print("====================================================")
+	print("    If you have any problems with this script,      ")
+	print("  please email a screenshot of your entire screen   ")
+	print(" with this console open to: chris@latenitefilms.com ")
+	print("====================================================")
+	
+	local finalCutProVersion = mod.finalCutProVersion()
+
+	local validFinalCutProVersion = false
+	if finalCutProVersion == "10.2.3" then
+		validFinalCutProVersion = true
+		require("hs.fcpx10-2-3")
+	end
+	if finalCutProVersion == "10.3" then
+		validFinalCutProVersion = true
+		require("hs.fcpx10-3")
+	end
+	if not validFinalCutProVersion then
+		mod.displayAlertMessage("We couldn't find a compatible version of Final Cut Pro installed on this system.\n\nPlease make sure it's installed in the Applications folder and hasn't been renamed.")
+	end
 end
 
 return mod
