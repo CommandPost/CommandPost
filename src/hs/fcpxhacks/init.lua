fb8e755669d579666b3914010b9ae9be646f1fba
Chris Hocking <chris@latenitefilms.com>
2016-12-21 11:14:50 +1100

    Added Hammerspoon Version to Console

--- src/hs/fcpxhacks/init.lua
+++ src/hs/fcpxhacks/init.lua
@@ -271,12 +271,12 @@ function mod.init()
 	--------------------------------------------------------------------------------
 	-- Display Useful Debugging Information in Console:
 	--------------------------------------------------------------------------------
-	if osVersion ~= nil then 					writeToConsole("macOS Version: " .. tostring(osVersion), true) 								end
-	if fcpVersion ~= nil then					writeToConsole("Final Cut Pro Version: " .. tostring(fcpVersion), true)						end
-	if fcpLanguage ~= nil then 					writeToConsole("Final Cut Pro Language: " .. tostring(fcpLanguage), true)					end
-	if keycodes.currentLayout() ~= nil then 	writeToConsole("Current Keyboard Layout: " .. tostring(keycodes.currentLayout()), true) 	end
-
-	writeToConsole("", true)
+												writeToConsole("Hammerspoon Version: 			" .. hs.processInfo["version"], true)
+	if osVersion ~= nil then 					writeToConsole("macOS Version: 					" .. tostring(osVersion), true) 					end
+	if fcpVersion ~= nil then					writeToConsole("Final Cut Pro Version: 			" .. tostring(fcpVersion), true)					end
+	if fcpLanguage ~= nil then 					writeToConsole("Final Cut Pro Language: 		" .. tostring(fcpLanguage), true)					end
+	if keycodes.currentLayout() ~= nil then 	writeToConsole("Current Keyboard Layout: 		" .. tostring(keycodes.currentLayout()), true) 		end
+												writeToConsole("", true)
 
 	local validFinalCutProVersion = false
 	if fcpVersion == "10.2.3" then
