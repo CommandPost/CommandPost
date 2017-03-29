a4ca9676130165016efef782df29dfa7ae73e2e1
Chris Hocking <chris@latenitefilms.com>
2017-03-29 20:35:54 +1100

    #421
    - Added “Send Crash Data” option to General Preferences
    - Cleaned up General Preferences plugin

--- src/plugins/core/preferences/general.lua
+++ src/plugins/core/preferences/general.lua
@@ -4,6 +4,10 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+--- === plugins.core.preferences.general ===
+---
+--- General Preferences Panel.
+
 --------------------------------------------------------------------------------
 --
 -- EXTENSIONS:
@@ -32,94 +36,144 @@ local DEFAULT_DISPLAY_MENUBAR_AS_ICON 	= true
 --------------------------------------------------------------------------------
 local mod = {}
 
-	--------------------------------------------------------------------------------
-	-- RESET SETTINGS:
-	--------------------------------------------------------------------------------
-	function mod.resetSettings()
+--- plugins.core.preferences.general.resetSettings() -> none
+--- Function
+--- Resets all of the CommandPost Preferences to their default values.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.resetSettings()
 
-		local finalCutProRunning = fcp:isRunning()
+	local finalCutProRunning = fcp:isRunning()
 
-		local resetMessage = i18n("trashPreferences")
-		if finalCutProRunning then
-			resetMessage = resetMessage .. "\n\n" .. i18n("adminPasswordRequiredAndRestart")
-		else
-			resetMessage = resetMessage .. "\n\n" .. i18n("adminPasswordRequired")
+	local resetMessage = i18n("trashPreferences")
+	if finalCutProRunning then
+		resetMessage = resetMessage .. "\n\n" .. i18n("adminPasswordRequiredAndRestart")
+	else
+		resetMessage = resetMessage .. "\n\n" .. i18n("adminPasswordRequired")
+	end
+
+	if not dialog.displayYesNoQuestion(resetMessage) then
+		return
+	end
+
+	--------------------------------------------------------------------------------
+	-- Remove Hacks Shortcut in Final Cut Pro:
+	--------------------------------------------------------------------------------
+	mod.hacksShortcuts.disableHacksShortcuts()
+
+	--------------------------------------------------------------------------------
+	-- Trash all Script Settings:
+	--------------------------------------------------------------------------------
+	config.reset()
+
+	--------------------------------------------------------------------------------
+	-- Restart Final Cut Pro if running:
+	--------------------------------------------------------------------------------
+	if finalCutProRunning then
+		if not fcp:restart() then
+			--------------------------------------------------------------------------------
+			-- Failed to restart Final Cut Pro:
+			--------------------------------------------------------------------------------
+			dialog.displayMessage(i18n("restartFinalCutProFailed"))
 		end
-
-		if not dialog.displayYesNoQuestion(resetMessage) then
-			return
-		end
-
-		--------------------------------------------------------------------------------
-		-- Remove Hacks Shortcut in Final Cut Pro:
-		--------------------------------------------------------------------------------
-		plugins("cp.plugins.finalcutpro.hacks.shortcuts").disableHacksShortcuts()
-
-		--------------------------------------------------------------------------------
-		-- Trash all Script Settings:
-		--------------------------------------------------------------------------------
-		config.reset()
-
-		--------------------------------------------------------------------------------
-		-- Restart Final Cut Pro if running:
-		--------------------------------------------------------------------------------
-		if finalCutProRunning then
-			if not fcp:restart() then
-				--------------------------------------------------------------------------------
-				-- Failed to restart Final Cut Pro:
-				--------------------------------------------------------------------------------
-				dialog.displayMessage(i18n("restartFinalCutProFailed"))
-			end
-		end
-
-		--------------------------------------------------------------------------------
-		-- Reload Hammerspoon:
-		--------------------------------------------------------------------------------
-		console.clearConsole()
-		hs.reload()
-
 	end
 
 	--------------------------------------------------------------------------------
-	-- TOGGLE DEVELOPER MODE:
+	-- Reload Hammerspoon:
 	--------------------------------------------------------------------------------
-	function mod.toggleDeveloperMode()
-		local debugMode = config.get("debugMode")
-		config.set("debugMode", not debugMode)
-		console.clearConsole()
-		hs.reload()
-	end
+	console.clearConsole()
+	hs.reload()
 
-	--------------------------------------------------------------------------------
-	-- TOGGLE DISPLAY MENUBAR AS ICON:
-	--------------------------------------------------------------------------------
-	function mod.toggleDisplayMenubarAsIcon()
-		local displayMenubarAsIcon = config.get("displayMenubarAsIcon", DEFAULT_DISPLAY_MENUBAR_AS_ICON)
-		config.set("displayMenubarAsIcon", not displayMenubarAsIcon)
-		mod.menuManager:updateMenubarIcon()
-	end
+end
 
-	--------------------------------------------------------------------------------
-	-- GET DEVELOPER MODE VALUE:
-	--------------------------------------------------------------------------------
-	function mod.getDeveloperMode()
-		return config.get("debugMode")
-	end
+--- plugins.core.preferences.general.toggleDeveloperMode() -> none
+--- Function
+--- Toggles the Developer Mode.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.toggleDeveloperMode()
+	local debugMode = config.get("debugMode")
+	config.set("debugMode", not debugMode)
+	console.clearConsole()
+	hs.reload()
+end
 
-	--------------------------------------------------------------------------------
-	-- GET DISPLAY MENUBAR AS ICON VALUE:
-	--------------------------------------------------------------------------------
-	function mod.getDisplayMenubarAsIcon()
-		return config.get("displayMenubarAsIcon", DEFAULT_DISPLAY_MENUBAR_AS_ICON)
-	end
+--- plugins.core.preferences.general.toggleDisplayMenubarAsIcon() -> none
+--- Function
+--- Toggles the menubar display icon from icon to text value and vice versa.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.toggleDisplayMenubarAsIcon()
+	local displayMenubarAsIcon = config.get("displayMenubarAsIcon", DEFAULT_DISPLAY_MENUBAR_AS_ICON)
+	config.set("displayMenubarAsIcon", not displayMenubarAsIcon)
+	mod.menuManager:updateMenubarIcon()
+end
 
-	--------------------------------------------------------------------------------
-	-- TOGGLE AUTO LAUNCH:
-	--------------------------------------------------------------------------------
-	function mod.toggleAutoLaunch()
-		hs.autoLaunch(not mod._autoLaunch)
-		mod._autoLaunch = not mod._autoLaunch
-	end
+--- plugins.core.preferences.general.getDeveloperMode() -> boolean
+--- Function
+--- Returns the Developer Mode status.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * `true` if developer mode is enabled otherwise `false`.
+function mod.getDeveloperMode()
+	return config.get("debugMode")
+end
+
+--- plugins.core.preferences.general.getDisplayMenubarAsIcon() -> boolean
+--- Function
+--- Returns whether the menubar is display as an icon or not.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * `true` if set to display the menubar as an icon other `false` if displaying menubar as text.
+function mod.getDisplayMenubarAsIcon()
+	return config.get("displayMenubarAsIcon", DEFAULT_DISPLAY_MENUBAR_AS_ICON)
+end
+
+--- plugins.core.preferences.general.toggleAutoLaunch() -> boolean
+--- Function
+--- Toggles the "Launch on Login" status for CommandPost.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.toggleAutoLaunch()
+	hs.autoLaunch(not mod._autoLaunch)
+	mod._autoLaunch = not mod._autoLaunch
+end
+
+--- plugins.core.preferences.general.toggleUploadCrashData() -> boolean
+--- Function
+--- Toggles the "Upload Crash Data" status for CommandPost.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.toggleUploadCrashData()
+	hs.uploadCrashData(not mod._uploadCrashData)
+	mod._uploadCrashData = not mod._uploadCrashData
+end
 
 --------------------------------------------------------------------------------
 --
@@ -133,62 +187,76 @@ local plugin = {
 		["core.preferences.panels.general"]	= "general",
 		["core.preferences.panels.menubar"]	= "menubar",
 		["core.menu.manager"]				= "menuManager",
+		["finalcutpro.hacks.shortcuts"] 	= "hacksShortcuts",
 	}
 }
+--------------------------------------------------------------------------------
+-- INITIALISE PLUGIN:
+--------------------------------------------------------------------------------
+function plugin.init(deps)
+
 	--------------------------------------------------------------------------------
-	-- INITIALISE PLUGIN:
+	-- Setup Dependencies:
 	--------------------------------------------------------------------------------
-	function plugin.init(deps)
+	mod.menuManager 		= deps.menuManager
+	mod.hacksShortcuts 		= deps.hacksShortcuts
 
-		mod.menuManager = deps.menuManager
+	--------------------------------------------------------------------------------
+	-- Cache Values:
+	--------------------------------------------------------------------------------
+	mod._autoLaunch 		= hs.autoLaunch()
+	mod._uploadCrashData 	= hs.uploadCrashData()
 
-		--------------------------------------------------------------------------------
-		-- Cache Auto Launch:
-		--------------------------------------------------------------------------------
-		mod._autoLaunch = hs.autoLaunch()
+	--------------------------------------------------------------------------------
+	-- Setup General Preferences Panel:
+	--------------------------------------------------------------------------------
+	deps.general:addHeading(1, function()
+		return { title = i18n("general") .. ":" }
+	end)
 
-		--------------------------------------------------------------------------------
-		-- Setup General Preferences Panel:
-		--------------------------------------------------------------------------------
-		deps.general:addHeading(1, function()
-			return { title = "General:" }
-		end)
+	:addCheckbox(3, function()
+		return { title = i18n("launchAtStartup"), fn = mod.toggleAutoLaunch, checked = mod._autoLaunch }
+	end)
 
-		:addCheckbox(3, function()
-			return { title = i18n("launchAtStartup"), fn = mod.toggleAutoLaunch, checked = mod._autoLaunch }
-		end)
+	:addHeading(50, function()
+		return { title = "<br />" .. i18n("privacy") .. ":" }
+	end)
 
-		:addHeading(50, function()
-			return { title = "<br />Developer:" }
-		end)
+	:addCheckbox(51, function()
+		return { title = i18n("sendCrashData"),	fn = mod.toggleUploadCrashData, checked = mod._uploadCrashData }
+	end)
 
-		:addCheckbox(51, function()
-			return { title = i18n("enableDeveloperMode"),	fn = mod.toggleDeveloperMode, checked = mod.getDeveloperMode() }
-		end)
+	:addHeading(60, function()
+		return { title = "<br />" .. i18n("advanced") .. ":" }
+	end)
 
-		:addButton(52, function()
-			return { title = i18n("openErrorLog"),	fn = function() hs.openConsole() end }
-		end, 150)
+	:addCheckbox(61, function()
+		return { title = i18n("enableDeveloperMode"),	fn = mod.toggleDeveloperMode, checked = mod.getDeveloperMode() }
+	end)
 
-		:addButton(53, function()
-			return { title = i18n("trashPreferences"),	fn = mod.resetSettings }
-		end, 150)
+	:addButton(62, function()
+		return { title = i18n("openErrorLog"),	fn = function() hs.openConsole() end }
+	end, 150)
 
-		--------------------------------------------------------------------------------
-		-- Setup Menubar Preferences Panel:
-		--------------------------------------------------------------------------------
-		deps.menubar:addHeading(20, function()
-			return { title = "Appearance:" }
-		end)
+	:addButton(63, function()
+		return { title = i18n("trashPreferences"),	fn = mod.resetSettings }
+	end, 150)
 
-		:addCheckbox(21, function()
-			return { title = i18n("displayThisMenuAsIcon"),	fn = mod.toggleDisplayMenubarAsIcon, checked = mod.getDisplayMenubarAsIcon() }
-		end)
+	--------------------------------------------------------------------------------
+	-- Setup Menubar Preferences Panel:
+	--------------------------------------------------------------------------------
+	deps.menubar:addHeading(20, function()
+		return { title = i18n("appearance") .. ":" }
+	end)
 
-		:addHeading(24, function()
-			return { title = "<br />Sections:" }
-		end)
+	:addCheckbox(21, function()
+		return { title = i18n("displayThisMenuAsIcon"),	fn = mod.toggleDisplayMenubarAsIcon, checked = mod.getDisplayMenubarAsIcon() }
+	end)
 
-	end
+	:addHeading(24, function()
+		return { title = "<br />" .. i18n("sections") .. ":" }
+	end)
+
+end
 
 return plugin
