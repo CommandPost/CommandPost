1403627b883b9afbc8a2c27694fd5ea24e160e5a
Chris Hocking <chris@latenitefilms.com>
2017-04-08 17:10:37 +1000

    Moved Setup Files
    - `setup.lua` and `_coresetup/init.lua` will now be stored in the main
    CommandPost repository
    - Removed custom `hs.alert` as it’s been updated on the main
    Hammerspoon branch

---
+++ src/setup/setup.lua
@@ -0,0 +1,23 @@
+local modpath, prettypath, fullpath, configdir, docstringspath, hasinitfile, autoload_extensions = ...
+
+print("-- Augmenting require paths")
+
+-- CommandPost Paths:
+package.path=configdir.."/?.lua"..";"..configdir.."/?/init.lua"..";"..modpath.."/?.lua"..";"..modpath.."/?/init.lua"
+package.cpath=configdir.."/?.so"..";"..modpath.."/?.so"
+
+-- Original Hammerspoon Paths:
+-- package.path=configdir.."/?.lua"..";"..configdir.."/?/init.lua"..";"..configdir.."/Spoons/?.spoon/init.lua"..";"..package.path..";"..modpath.."/?.lua"..";"..modpath.."/?/init.lua"
+-- package.cpath=configdir.."/?.so"..";"..package.cpath..";"..modpath.."/?.so"
+
+print("-- package.path:")
+for part in string.gmatch(package.path, "([^;]+)") do
+  print("      "..part)
+end
+
+print("-- package.cpath:")
+for part in string.gmatch(package.cpath, "([^;]+)") do
+  print("      "..part)
+end
+
+return require'hs._coresetup'.setup(...)
