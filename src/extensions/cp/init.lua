661f1d72789f9030040cb4bc14bd9fcaf98097d4
Chris Hocking <chris@latenitefilms.com>
2017-04-09 17:41:26 +1000

    #435
    - CommandPost now remembers if the Error Log was open and it’s last
    position between restarts

--- src/extensions/cp/init.lua
+++ src/extensions/cp/init.lua
@@ -9,17 +9,13 @@
 --- Core CommandPost functionality
 
 --------------------------------------------------------------------------------
--- SET UP LOGGER:
---------------------------------------------------------------------------------
-local logger					= require("hs.logger")
-logger.defaultLogLevel 			= 'debug'
-local log						= require("hs.logger").new("cp")
-
---------------------------------------------------------------------------------
 --
 -- EXTENSIONS:
 --
 --------------------------------------------------------------------------------
+local logger					= require("hs.logger"); logger.defaultLogLevel = 'debug'
+local log						= logger.new("cp")
+
 local console                   = require("hs.console")
 local drawing                   = require("hs.drawing")
 local fs                        = require("hs.fs")
@@ -31,26 +27,6 @@ local toolbar                   = require("hs.webview.toolbar")
 local config					= require("cp.config")
 local tools                     = require("cp.tools")
 local plugins					= require("cp.plugins")
-
---------------------------------------------------------------------------------
--- SHUTDOWN CALLBACK:
---------------------------------------------------------------------------------
-hs.shuttingDown = false
-function hs.shutdownCallback()
-	hs.shuttingDown = true
-	console.clearConsole()
-end
-
---------------------------------------------------------------------------------
--- DEBUG MODE:
---------------------------------------------------------------------------------
-local debugMode 				= config.get("debugMode")
-if debugMode then
-    logger.defaultLogLevel = 'debug'
-	require("cp.developer")
-else
-	logger.defaultLogLevel = 'warning'
-end
 
 --------------------------------------------------------------------------------
 -- SETUP I18N LANGUAGES:
@@ -69,41 +45,6 @@ else
 	userLocale = config.get("language")
 end
 i18n.setLocale(userLocale)
-
---------------------------------------------------------------------------------
--- ADD TOOLBAR TO ERROR LOG:
---------------------------------------------------------------------------------
-function consoleOnTopIcon()
-	if hs.consoleOnTop() then
-		return image.imageFromName("NSStatusAvailable")
-	else
-		return image.imageFromName("NSStatusUnavailable")
-	end
-end
-local toolbar = require("hs.webview.toolbar")
-errorLogToolbar = toolbar.new("myConsole", {
-		{ id = i18n("reload"), image = image.imageFromName("NSPreferencesGeneral"),
-			fn = function()
-				console.clearConsole()
-				print("Reloading CommandPost...")
-				hs.reload()
-			end
-		},
-		{ id = i18n("clearLog"), image = image.imageFromName("NSTrashFull"),
-			fn = function()
-				console.clearConsole()
-			end
-		},
-		{ id = i18n("alwaysOnTop"), image = consoleOnTopIcon(),
-			fn = function()
-				hs.consoleOnTop(not hs.consoleOnTop())
-				errorLogToolbar:modifyItem({id = i18n("alwaysOnTop"), image = consoleOnTopIcon()})
-			end
-		},
-    })
-	:canCustomize(true)
-    :autosaves(true)
-console.toolbar(errorLogToolbar)
 
 --------------------------------------------------------------------------------
 -- EXTENSIONS (THAT REQUIRE i18N):
@@ -129,6 +70,80 @@ local mod = {}
 --- Returns:
 ---  * None
 function mod.init()
+
+	--------------------------------------------------------------------------------
+	-- Debug Mode:
+	--------------------------------------------------------------------------------
+	local debugMode = config.get("debugMode")
+	if debugMode then
+		logger.defaultLogLevel = 'debug'
+		require("cp.developer")
+	else
+		logger.defaultLogLevel = 'warning'
+	end
+
+	--------------------------------------------------------------------------------
+	-- Add Toolbar To Error Log:
+	--------------------------------------------------------------------------------
+	function consoleOnTopIcon()
+		if hs.consoleOnTop() then
+			return image.imageFromName("NSStatusAvailable")
+		else
+			return image.imageFromName("NSStatusUnavailable")
+		end
+	end
+	local toolbar = require("hs.webview.toolbar")
+	errorLogToolbar = toolbar.new("myConsole", {
+			{ id = i18n("reload"), image = image.imageFromName("NSPreferencesGeneral"),
+				fn = function()
+					console.clearConsole()
+					print("Reloading CommandPost...")
+					hs.reload()
+				end
+			},
+			{ id = i18n("clearLog"), image = image.imageFromName("NSTrashFull"),
+				fn = function()
+					console.clearConsole()
+				end
+			},
+			{ id = i18n("alwaysOnTop"), image = consoleOnTopIcon(),
+				fn = function()
+					hs.consoleOnTop(not hs.consoleOnTop())
+					errorLogToolbar:modifyItem({id = i18n("alwaysOnTop"), image = consoleOnTopIcon()})
+				end
+			},
+		})
+		:canCustomize(true)
+		:autosaves(true)
+	console.toolbar(errorLogToolbar)
+
+	--------------------------------------------------------------------------------
+	-- Open Error Log:
+	--------------------------------------------------------------------------------
+	local errorLogOpenOnClose = config.get("errorLogOpenOnClose", false)
+	if errorLogOpenOnClose then
+		hs.openConsole()
+		local lastErrorLogFrame = config.get("lastErrorLogFrame", nil)
+		if lastErrorLogFrame then
+			local frame = hs.geometry.rect(lastErrorLogFrame["_x"], lastErrorLogFrame["_y"], lastErrorLogFrame["_w"], lastErrorLogFrame["_h"])
+			console.hswindow():setFrame(frame)
+		end
+	end
+
+	--------------------------------------------------------------------------------
+	-- Shutdown Callback:
+	--------------------------------------------------------------------------------
+	hs.shuttingDown = false
+	hs.shutdownCallback = function()
+		hs.shuttingDown = true
+		if console.hswindow() then
+			config.set("errorLogOpenOnClose", true)
+			config.set("lastErrorLogFrame", console.hswindow():frame())
+		else
+			config.set("errorLogOpenOnClose", false)
+		end
+		console.clearConsole()
+	end
 
 	--------------------------------------------------------------------------------
 	-- Check Versions & Language:
