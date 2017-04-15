3e6ed759b5f72db0b7e03f94e9a522440f8c6944
Chris Hocking <chris@latenitefilms.com>
2017-04-16 03:11:29 +1000

    Updated Docs Script

--- scripts/build_commandpost_docs.sh
+++ scripts/build_commandpost_docs.sh
@@ -2,4 +2,8 @@
 
 cd ../CommandPost-App/
 
-scripts/docs/bin/build_docs.py -o ../CommandPost-Release/Docs/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/
+scripts/docs/bin/build_docs.py -o ../CommandPost-Releases/Docs/cp.finalcutpro/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/finalcutpro/
+scripts/docs/bin/build_docs.py -o ../CommandPost-Releases/Docs/cp.just/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/just/
+scripts/docs/bin/build_docs.py -o ../CommandPost-Releases/Docs/cp.bench/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/bench/
+scripts/docs/bin/build_docs.py -o ../CommandPost-Releases/Docs/cp.choices/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/choices/
+scripts/docs/bin/build_docs.py -o ../CommandPost-Releases/Docs/cp.commands/ --standalone --debug --markdown ../CommandPost/src/extensions/cp/commands/
