63d8cf7f981d7b2ec0392325601198e766be3979
Chris Hocking <chris@latenitefilms.com>
2017-04-17 19:51:11 +1000

    Added Dialog Box when Toggling Developer Mode

--- src/extensions/cp/resources/languages/en.lua
+++ src/extensions/cp/resources/languages/en.lua
@@ -429,6 +429,7 @@ return {
 			developer							=			"Developer",
 			commandLineTool						=			"Command Line Tool",
 			enableDeveloperMode					=			"Enable Developer Mode",
+			togglingDeveloperMode				=			"Toggling Developer Mode will require CommandPost to reload.\n\nDo you want to continue?",
 			trashPreferences					=			"Trash Preferences",
 			cliUninstallError					=			"I'm sorry, but we failed to Uninstall the Command Line Tool. Please try again.",
 			cliInstallError						=			"I'm sorry, but we ffailed to Install the Command Line Tool. Please try again.",
@@ -517,58 +518,58 @@ return {
 			rectangle							= 			"Rectangle",
 			circle								=			"Circle",
 			diamond								=			"Diamond",
-			
+
 			--------------------------------------------------------------------------------
 			-- Preference Panels:
 			--------------------------------------------------------------------------------
 			generalPanelLabel					=			"General",
 			generalPanelTooltip					=			"General Preferences",
-			
+
 			menubarPanelLabel					=			"Menubar",
 			menubarPanelTooltip					=			"Menubar Preferences",
-			
+
 			advancedPanelLabel					=			"Advanced",
 			advancedPanelTooltip				=			"Advanced Preferences",
-			
+
 			finalCutProPanelLabel				=			"Final Cut Pro",
 			finalCutProPanelTooltip				=			"Final Cut Pro Preferences",
-			
+
 			pluginsPanelLabel					=			"Plugins",
 			pluginsPanelTooltip					=			"Plugins Preferences",
-			
+
 			pluginsManagerLabel					=			"Plugins Manager",
 			pluginCategory						=			"Category",
 			pluginName							=			"Plugin Name",
 			pluginStatus						=			"Status",
 			pluginAction						=			"Action",
-			
+
 			pluginsCustomFolderDetails			=			[[<strong>Custom Plugins</strong> can also be saved in the Plugins Folder.]],
 			pluginsOpenPluginsFolder			=			"Open Plugins Folder",
-			
+
 			setupHeading						=			"Setup",
 			menubarHeading						=			"Menubar",
-			
+
 			languageHeading						=			"Language",
 			commandPostLanguage					=			"CommandPost Language",
-			
+
 			shortcutsPanelLabel					=			"Shortcuts",
 			shortcutsPanelTooltip				=			"Keyboard Shortcuts",
-			
+
 			webappPanelLabel					=			"WebApp",
 			webappPanelTooltip					=			"WebApp Preferences",
-			
+
 			webappIntroduction					=			"Introduction",
-			
+
 			webappInstructions					=			[[
 			The <strong>WebApp</strong> is a very easy way to control CommandPost via your mobile phone or tablet.<br /><br />
 			All you need to do is connect your device to the same network as this machine, enable the WebApp below, then access the WebApp via your devices browser by entering URL below.
 			]],
-			
+
 			webappSettings						=			"Settings",
 			webappEnable						=			"Enable WebApp",
 			webappHostname						=			"Hostname",
 			webappUnresolvedHostname			=			"Failed to Resolve Hostname!",
-			
+
 			--------------------------------------------------------------------------------
 			-- Hammerspoon Settings:
 			--------------------------------------------------------------------------------
@@ -689,13 +690,13 @@ return {
 
 	plugin_group_core					=			"Core",
 	plugin_group_finalcutpro			=			"Final Cut Pro",
-	
+
 	--------------------------------------------------------------------------------
 	-- SHORTCUT PANEL:
 	--------------------------------------------------------------------------------
 	shortcut_group_global				=			"Global",
 	shortcut_group_fcpx					=			"FCPX",
-	
+
 	customiseShortcuts					=			"Customise Shortcuts",
 	shortcutsGroup						=			"Group",
 	shortcutsLabel						=			"Label",
