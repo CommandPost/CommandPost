db8078416e939132d78e2068815247002e360d4c
Chris Hocking <latenitefilms@users.noreply.github.com>
2017-09-07 23:12:07 +1000

    Work in progress

--- src/plugins/finalcutpro/timeline/magicmousezoom/init.lua
+++ src/plugins/finalcutpro/timeline/magicmousezoom/init.lua
@@ -36,6 +36,10 @@ local ENABLED_DEFAULT 	= false
 --------------------------------------------------------------------------------
 local mod = {}
 
+mod.touchDevices = {}
+mod.magicMouseIDs = {}	
+mod.numberOfTouchDevices = 0
+
 --- plugins.finalcutpro.timeline.magicmousezoom.update() -> none
 --- Function
 --- Checks to see whether or not we should enable the timeline zoom watchers.
@@ -58,6 +62,94 @@ end
 --- Toggles the Enable Proxy Menu Icon
 mod.enabled = config.prop("enablemagicmousezoom", ENABLED_DEFAULT):watch(mod.update)
 
+--- plugins.finalcutpro.timeline.magicmousezoom.stop() -> none
+--- Function
+--- Disables the ability to zoom a timeline using your mouse scroll wheel and the OPTION modifier key.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.stop()
+	-- Clear any existing existing Touch Devices:
+	if mod.touchDevices then		
+		for i=0, #mod.touchDevices do 
+			mod.touchDevices[i] = nil 
+		end
+		mod.touchDevices = nil
+	end
+end
+
+--- plugins.finalcutpro.timeline.magicmousezoom.findMagicMouses() -> none
+--- Function
+--- Find Magic Mouse Devices and adds them to a table.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function mod.findMagicMouses()
+
+	-- Clear any existing existing Touch Devices:
+	mod.stop()
+	
+	if touchdevice.available() then	
+		mod.magicMouseIDs = {}	
+		local devices = touchdevice.devices()
+		mod.numberOfTouchDevices = #devices
+		if devices then 
+			for _, id in ipairs(devices) do		
+				local selectedDevice = touchdevice.forDeviceID(id)
+				if selectedDevice then
+					local selectedProductName = selectedDevice:details().productName 				
+					if selectedProductName == "Magic Mouse" or selectedProductName == "Magic Mouse 2" then
+						log.df("Found a Magic Mouse! ID: %s", id)						
+						mod.magicMouseIDs[#mod.magicMouseIDs + 1] = id
+					end
+				end		
+			end
+		end
+	end
+end
+
+mod.timeInterval = 0.09
+
+local function touchCallback(self, touches, time, frame)
+	local currentTime = touchdevice.absoluteTime()
+	if not mod.absoluteTime then mod.absoluteTime = currentTime end
+	if #touches == 1 and touches[1].stage == "touching" and currentTime > mod.absoluteTime + mod.timeInterval then -- Only trigger when one finger is detected.
+		local mods = eventtap.checkKeyboardModifiers()
+		local mouseButtons = eventtap.checkMouseButtons()
+		if mods['alt'] and not mods['cmd'] and not mods['shift'] and not mods['ctrl'] and not mods['capslock'] and not mods['fn'] and not next(mouseButtons) and fcp.isFrontmost() and fcp:timeline():isShowing() then
+		
+			-- TODO: Need to work out how best to work out the scrolling direction.
+			
+			log.df("touchdevice.absoluteTime(): %s", touchdevice.absoluteTime())
+			
+			
+			
+			--print("Touch Data: " .. hs.inspect(touches))
+			
+			currentValue = touches[1].absoluteVector.position.x
+
+			if lastValue then 
+				if lastValue > currentValue then
+					log.df("Zoom In")
+					fcp:selectMenu({"View", "Zoom In"})
+				else
+					log.df("Zoom Out")
+					fcp:selectMenu({"View", "Zoom Out"})
+				end
+			end										
+			lastValue = currentValue
+			
+			mod.absoluteTime = currentTime					
+		end
+	end
+end			
+
 --- plugins.finalcutpro.timeline.magicmousezoom.start() -> none
 --- Function
 --- Enables the ability to zoon a timeline using your mouse scroll wheel and the OPTION modifier key.
@@ -67,60 +159,14 @@ mod.enabled = config.prop("enablemagicmousezoom", ENABLED_DEFAULT):watch(mod.upd
 ---
 --- Returns:
 ---  * None
-function mod.start()
-	
-	if touchdevice.available() then	
-	
-		-- TODO: We need to actually pick the Magic Mouse, not just the first Touchpad Device:
-		
-		local devices = touchdevice.devices()
-		local deviceID = devices[1]
-		
-		local lastValue = nil
-		
-		mod.touchdevice = touchdevice.forDeviceID(deviceID):frameCallback(function(self, touches, time, frame)
-			if #touches == 2 then -- Only trigger when two fingers are detected.
-				local mods = eventtap.checkKeyboardModifiers()
-				local mouseButtons = eventtap.checkMouseButtons()
-				if mods['alt'] and not mods['cmd'] and not mods['shift'] and not mods['ctrl'] and not mods['capslock'] and not mods['fn'] and not next(mouseButtons) and fcp.isFrontmost() and fcp:timeline():isShowing() then
-				
-					-- TODO: Need to work out how best to work out the scrolling direction.
-					
-					--print("Touch Data: " .. hs.inspect(touches))
-					
-					currentValue = touches[1].normalizedVector.position.x
-
-					if lastValue then 
-						if lastValue > currentValue then
-							log.df("Zoom In")
-							fcp:selectMenu({"View", "Zoom In"})
-						else
-							log.df("Zoom Out")
-							fcp:selectMenu({"View", "Zoom Out"})
-						end
-					end										
-					lastValue = currentValue					
-				end
-			end
-		end):start()
-	end
-			
-end
-
---- plugins.finalcutpro.timeline.magicmousezoom.stop() -> none
---- Function
---- Disables the ability to zoon a timeline using your mouse scroll wheel and the OPTION modifier key.
----
---- Parameters:
----  * None
----
---- Returns:
----  * None
-function mod.stop()
-	if mod.touchdevice then
-		mod.touchdevice:stop()
-		mod.touchdevice = nil
-	end
+function mod.start()		
+	mod.findMagicMouses()
+	if not mod.touchDevices then mod.touchDevices = {} end
+	if mod.numberOfTouchDevices >= 1 then 	
+		for _, id in ipairs(mod.magicMouseIDs) do					
+			mod.touchDevices[id] = touchdevice.forDeviceID(id):frameCallback(touchCallback):start()		
+		end		
+	end	
 end
 
 --------------------------------------------------------------------------------
