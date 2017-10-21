8fb0f8d13857752daaeb0994252e8ffe25e7b2e0
David Peterson <david@randombits.org>
2017-10-21 18:21:15 +1000

    #380 Only outputs individual tests when there is a failure

--- src/extensions/cp/test/init.lua
+++ src/extensions/cp/test/init.lua
@@ -1,27 +1,50 @@
-local test = require("gambiarra")
-
+local test		= require("gambiarra")
 local log		= require("hs.logger").new("test")
+
+local format, insert	= string.format, table.insert
 
 local passed = 0
 local failed = 0
 local clock = 0
 
+local messages = nil
+
+local function f(message, ...)
+	if not messages then
+		messages = {}
+	end
+	insert(messages, format(message, ...))
+end
+
+local function dumpMessages()
+	if messages then
+		for _,message in ipairs(messages) do
+			log.f(message)
+		end
+	end
+end
+
 test(function(event, testfunc, msg)
-    if event == 'begin' then
-        log.f(' [START] %s', testfunc)
+	if event == 'begin' then
+		messages = nil
+        -- f(' [START] %s', testfunc)
         passed = 0
         failed = 0
         clock = os.clock()
     elseif event == 'end' then
-        log.f('   [END] %s', testfunc)
-		log.f("[RESULT] Passed: %d; Failed: %d; Time: %.4f\n", passed, failed, os.clock() - clock)
+		-- f('   [END] %s', testfunc)
+		if failed > 0 then
+			dumpMessages()
+			log.f('[RESULT] "%s": Passed: %d; Failed: %d; Time: %.4f\n', testfunc, passed, failed, os.clock() - clock)
+		end
     elseif event == 'pass' then
         passed = passed + 1
     elseif event == 'fail' then
-        log.f('  [FAIL] %s', msg)
+        f('  [FAIL] "%s": %s', testfunc, msg)
         failed = failed + 1
     elseif event == 'except' then
-        log.f(' [ERROR] "%s": %s', testfunc, msg)
+		f(' [ERROR] "%s": %s', testfunc, msg)
+		failed = failed + 1
     end
 end)
 
