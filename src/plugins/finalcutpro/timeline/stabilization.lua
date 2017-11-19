55b6ec42b65a726f3ec686e3b71568bfd3bdb135
Chris Hocking <latenitefilms@users.noreply.github.com>
2017-11-19 13:02:08 +1100

    #637
    - Added Enable/Disable/Toggle Stabilization Action
    - Closes #637

---
+++ src/plugins/finalcutpro/timeline/stabilization.lua
@@ -0,0 +1,80 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--                   C  O  M  M  A  N  D  P  O  S  T                          --
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--- === plugins.finalcutpro.timeline.stabilization ===
+---
+--- Stabilization Shortcut
+
+--------------------------------------------------------------------------------
+--
+-- EXTENSIONS:
+--
+--------------------------------------------------------------------------------
+local log							= require("hs.logger").new("stabilization")
+
+local fcp							= require("cp.apple.finalcutpro")
+
+--------------------------------------------------------------------------------
+--
+-- THE MODULE:
+--
+--------------------------------------------------------------------------------
+local mod = {}
+
+function mod.stabilization(value)
+
+	--------------------------------------------------------------------------------
+	-- Set Stabilization:
+	--------------------------------------------------------------------------------
+	local inspector = fcp:inspector()
+	if type(value) == "boolean" then
+		inspector:stabilization(value)
+	else
+		--------------------------------------------------------------------------------
+		-- Toggle:
+		--------------------------------------------------------------------------------
+		local value = inspector:stabilization()
+		inspector:stabilization(not value)
+	end
+
+end
+
+--------------------------------------------------------------------------------
+--
+-- THE PLUGIN:
+--
+--------------------------------------------------------------------------------
+local plugin = {
+	id = "finalcutpro.timeline.stabilization",
+	group = "finalcutpro",
+	dependencies = {
+		["finalcutpro.commands"]			= "fcpxCmds",
+	}
+}
+
+function plugin.init(deps)
+
+	--------------------------------------------------------------------------------
+	-- Commands:
+	--------------------------------------------------------------------------------
+	local cmds = deps.fcpxCmds
+
+	cmds:add("cpStabilizationToggle")
+		:groupedBy("timeline")
+		:whenActivated(function() mod.stabilization() end)
+
+	cmds:add("cpStabilizationEnable")
+		:groupedBy("timeline")
+		:whenActivated(function() mod.stabilization(true) end)
+
+	cmds:add("cpStabilizationDisable")
+		:groupedBy("timeline")
+		:whenActivated(function() mod.stabilization(false) end)
+
+	return mod
+end
+
+return plugin
