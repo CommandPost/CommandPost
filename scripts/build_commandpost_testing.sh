7af27ba8c5f12d38c55f1198369ed8ebdb74baaa
Cail Young <cailyoung+github@gmail.com>
2018-01-02 22:00:39 +1100

    refactor dev build script to use librelease.sh

--- scripts/build_commandpost_testing.sh
+++ scripts/build_commandpost_testing.sh
@@ -6,15 +6,22 @@ set -o pipefail
 # Compile CommandPost:
 #
 
-cd ..
-cd ..
-cd CommandPost-App/ ||  echo "Couldn't cd, exiting" && exit
+# set directory variables
+# export SCRIPT_NAME
+export SCRIPT_HOME
+export HAMMERSPOON_HOME
 
+# SCRIPT_NAME="$(basename "$0")"
+SCRIPT_HOME="$(dirname "$(greadlink -f "$0")")" # fully qualified directory of this script
+HAMMERSPOON_HOME="$(greadlink -f "${SCRIPT_HOME}/../")" # fully qualified directory of the parent directiry of the script location
 
-make clean || echo "Make failed, exiting" && exit
-make release
-make docs
+# Import our function library
+# shellcheck source=scripts/librelease.sh disable=SC1091
+source "../CommandPost/scripts/inc/librelease.sh"
 
+build # run the build function from librelease
+
+# clean up the unsigned build
 rm -fr "$(xcodebuild -workspace Hammerspoon.xcworkspace -scheme Hammerspoon -configuration Release -showBuildSettings | sort | uniq | grep " BUILT_PRODUCTS_DIR =" | awk '{ print $3 }')/CommandPost.app"
 
 #
