5da584f1caa5e166e09989e320cc44190d566dfd
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-01-18 20:38:13 +1100

    #942
    - Removed tabs to keep @stickler-ci happy

--- src/extensions/cp/sourcewatcher/init.lua
+++ src/extensions/cp/sourcewatcher/init.lua
@@ -15,15 +15,10 @@
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
--- Logger:
---------------------------------------------------------------------------------
-local log				            = require("hs.logger").new("sourcewatcher")
-
---------------------------------------------------------------------------------
 -- Hammerspoon Extensions:
 --------------------------------------------------------------------------------
-local console						= require("hs.console")
-local pathwatcher					= require("hs.pathwatcher")
+local console                       = require("hs.console")
+local pathwatcher                   = require("hs.pathwatcher")
 
 --------------------------------------------------------------------------------
 --
@@ -40,18 +35,18 @@ mod.mt.__index = mod.mt
 --- Creates a new `sourcewatcher` instance.
 ---
 --- Parameters:
----  * `extensions`		- Extensions
+---  * `extensions`     - Extensions
 ---
 --- Returns:
 ---  * A sourcewatcher instance
 function mod.new(extensions)
-	local o = {
-		extensions = extensions,
-		paths = {},
-		watchers = {},
-	}
-	setmetatable(o, mod.mt)
-	return o
+    local o = {
+        extensions = extensions,
+        paths = {},
+        watchers = {},
+    }
+    setmetatable(o, mod.mt)
+    return o
 end
 
 --- cp.sourcewatcher:matchesExtensions(file) -> boolean
@@ -59,27 +54,27 @@ end
 --- Checks that the file that triggered the Path Watcher matches the extension given.
 ---
 --- Parameters:
----  * `file`		- The file as string
+---  * `file`       - The file as string
 ---
 --- Returns:
 ---  * A boolean value
 function mod.mt:matchesExtensions(file)
-	if not self.extensions then
-	    --------------------------------------------------------------------------------
-		-- Nothing specified, all files match:
-		--------------------------------------------------------------------------------
-		return true
-	else
-	    --------------------------------------------------------------------------------
-		-- Must match one of the extensions:
-		--------------------------------------------------------------------------------
-		for _,ext in ipairs(self.extensions) do
-			if file:sub(-1*ext:len()) == ext then
-				return true
-			end
-		end
-		return false
-	end
+    if not self.extensions then
+        --------------------------------------------------------------------------------
+        -- Nothing specified, all files match:
+        --------------------------------------------------------------------------------
+        return true
+    else
+        --------------------------------------------------------------------------------
+        -- Must match one of the extensions:
+        --------------------------------------------------------------------------------
+        for _,ext in ipairs(self.extensions) do
+            if file:sub(-1*ext:len()) == ext then
+                return true
+            end
+        end
+        return false
+    end
 end
 
 --- cp.sourcewatcher:filesChanged(files, flagTables) -> boolean
@@ -87,25 +82,25 @@ end
 --- Checks that the file that triggered the Path Watcher matches the extension given.
 ---
 --- Parameters:
----  * `files`		- Table of files to check as strings
+---  * `files`      - Table of files to check as strings
 ---  * `flagTables` - Table of flagTables (see: `hs.pathwatcher.new`)
 ---
 --- Returns:
 ---  * None
 function mod.mt:filesChanged(files, flagTables)
-	for i,file in ipairs(files) do
-		if self:matchesExtensions(file) then
-		    local flag = flagTables[i]
-		    --------------------------------------------------------------------------------
-		    -- Only reload if it's a file and it's been modified:
-		    --------------------------------------------------------------------------------
-		    if flag and flag.itemIsFile == true and flag.itemModified == true then
-	    		console.clearConsole()
-		    	hs.reload()
-		    end
-			return
-		end
-	end
+    for i,file in ipairs(files) do
+        if self:matchesExtensions(file) then
+            local flag = flagTables[i]
+            --------------------------------------------------------------------------------
+            -- Only reload if it's a file and it's been modified:
+            --------------------------------------------------------------------------------
+            if flag and flag.itemIsFile == true and flag.itemModified == true then
+                console.clearConsole()
+                hs.reload()
+            end
+            return
+        end
+    end
 end
 
 --- cp.sourcewatcher:watchPath(path) -> sourcewatcher
@@ -113,18 +108,18 @@ end
 --- Watches a path.
 ---
 --- Parameters:
----  * `path`		- The path you want to watch as a string.
+---  * `path`       - The path you want to watch as a string.
 ---
 --- Returns:
 ---  * sourcewatcher
 function mod.mt:watchPath(path)
-	self.watchers[#self.watchers + 1] = pathwatcher.new(
-		path,
-		function(files, flagTables)
-			self:filesChanged(files, flagTables)
-		end
-	):start()
-	return self
+    self.watchers[#self.watchers + 1] = pathwatcher.new(
+        path,
+        function(files, flagTables)
+            self:filesChanged(files, flagTables)
+        end
+    ):start()
+    return self
 end
 
 --- cp.sourcewatcher:stop() -> none
@@ -137,11 +132,11 @@ end
 --- Returns:
 ---  * None
 function mod.mt:stop()
-	if self.watchers then
-		for _,watcher in ipairs(self.watchers) do
-			watcher:stop()
-		end
-	end
+    if self.watchers then
+        for _,watcher in ipairs(self.watchers) do
+            watcher:stop()
+        end
+    end
 end
 
 --- cp.sourcewatcher:start() -> none
@@ -154,11 +149,11 @@ end
 --- Returns:
 ---  * None
 function mod.mt:start()
-	if self.watchers then
-		for _,watcher in ipairs(self.watchers) do
-			watcher:start()
-		end
-	end
+    if self.watchers then
+        for _,watcher in ipairs(self.watchers) do
+            watcher:start()
+        end
+    end
 end
 
 return mod
