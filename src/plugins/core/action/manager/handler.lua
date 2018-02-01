0047facaf79cfd3f6792754017a935d17c8bd568
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-02-01 12:08:05 +1100

    #1001

--- src/plugins/core/action/manager/handler.lua
+++ src/plugins/core/action/manager/handler.lua
@@ -20,12 +20,29 @@
 --- The choices added to the `choices` should have the `params` value set to a table
 --- containing the details of the action to execute if the choice is selected.
 
-local log				= require("hs.logger").new("actnhndlr")
+--------------------------------------------------------------------------------
+--
+-- EXTENSIONS:
+--
+--------------------------------------------------------------------------------
 
-local choices			= require("cp.choices")
-local config			= require("cp.config")
-local prop				= require("cp.prop")
+--------------------------------------------------------------------------------
+-- Logger:
+--------------------------------------------------------------------------------
+local log               = require("hs.logger").new("actnhndlr")
 
+--------------------------------------------------------------------------------
+-- CommandPost Extensions:
+--------------------------------------------------------------------------------
+local choices           = require("cp.choices")
+local config            = require("cp.config")
+local prop              = require("cp.prop")
+
+--------------------------------------------------------------------------------
+--
+-- THE MODULE:
+--
+--------------------------------------------------------------------------------
 local handler = {}
 
 handler.mt = {}
@@ -36,18 +53,18 @@ handler.mt.__index = handler.mt
 --- Creates a new handler with the specified ID.
 ---
 --- Parameters:
---- * `id`		- The unique ID of the action handler.
---- * `group`	- The group the handler belongs to.
+--- * `id`      - The unique ID of the action handler.
+--- * `group`   - The group the handler belongs to.
 ---
 --- Returns:
 --- * The new action handler instance.
 function handler.new(id, group)
-	local o = {
-		_id = id,
-		_group = group
-	}
+    local o = {
+        _id = id,
+        _group = group
+    }
 
-	return prop.extend(o, handler.mt)
+    return prop.extend(o, handler.mt)
 end
 
 --- plugins.core.action.handler:onExecute(executeFn) -> handler
@@ -56,13 +73,13 @@ end
 --- the choice parameters in a single table.
 ---
 --- Parameters:
---- * `executeFn`		- The function to call when executing.
+--- * `executeFn`       - The function to call when executing.
 ---
 --- Returns:
 --- * This action handler.
 function handler.mt:onExecute(executeFn)
-	self._onExecute = executeFn
-	return self
+    self._onExecute = executeFn
+    return self
 end
 
 --- plugins.core.action.handler:onChoices(choicesFn) -> handler
@@ -72,13 +89,13 @@ end
 --- if the [cached](#cached) property is set to `true`.
 ---
 --- Parameters:
---- * `choicesFn`		- The function with the signature of `function(choices) -> nothing`
+--- * `choicesFn`       - The function with the signature of `function(choices) -> nothing`
 ---
 --- Returns:
 --- * This action handler.
 function handler.mt:onChoices(choicesFn)
-	self._onChoices = choicesFn
-	return self
+    self._onChoices = choicesFn
+    return self
 end
 
 --- plugins.core.action.handler:onActionId(actionFn) -> handler
@@ -87,13 +104,13 @@ end
 --- The function is passed the `action` table and should return a string.
 ---
 --- Parameters:
---- * `actionFn`	- The function with a signature of `function(action) -> string`
+--- * `actionFn`    - The function with a signature of `function(action) -> string`
 ---
 --- Returns:
 --- * This action handler.
 function handler.mt:onActionId(actionFn)
-	self._onActionId = actionFn
-	return self
+    self._onActionId = actionFn
+    return self
 end
 
 --- plugins.core.action.handler:group() -> string
@@ -106,7 +123,7 @@ end
 --- Returns:
 --- * Group as string.
 function handler.mt:group()
-	return self._group
+    return self._group
 end
 
 --- plugins.core.action.handler:id() -> string
@@ -119,7 +136,7 @@ end
 --- Returns:
 --- * The ID string.
 function handler.mt:id()
-	return self._id
+    return self._id
 end
 
 --- plugins.core.action.handler.cached <cp.prop: boolean>
@@ -128,8 +145,8 @@ end
 handler.mt.cached = prop.TRUE()
 :bind(handler.mt)
 :watch(function(cached, self)
-	-- reset the cache
-	self:reset()
+    -- reset the cache
+    self:reset()
 end)
 
 --- plugins.core.action.handler.choices <cp.prop: cp.choices; read-only>
@@ -139,18 +156,18 @@ end)
 --- of values that identify the choice. This can be passed to the [execute](#execute)
 --- method to trigger the choice.
 handler.mt.choices = prop(function(self)
-	local result = self._choices
-	if not result then
-		result = choices.new(self:id())
-		-- populate the result
-		local incomplete = self._onChoices(result) == true
+    local result = self._choices
+    if not result then
+        result = choices.new(self:id())
+        -- populate the result
+        local incomplete = self._onChoices(result) == true
 
- 		-- cache if appropriate
-		if not incomplete and self:cached() then
-			self._choices = result
-		end
-	end
-	return result
+        -- cache if appropriate
+        if not incomplete and self:cached() then
+            self._choices = result
+        end
+    end
+    return result
 end)
 :bind(handler.mt)
 
@@ -159,23 +176,23 @@ end)
 -- Default handler for adding choices. Throws an error message.
 --
 -- Parameters:
--- * `choices`	- The `cp.choices` to add to.
+-- * `choices`  - The `cp.choices` to add to.
 --
 -- Returns:
 -- * Nothing
 function handler.mt:_onChoices(choices)
-	--log.df("unimplmemented: handler:onChoices(choicesFn)")
+    log.df("unimplemented: handler:onChoices(choicesFn)")
 end
 
 function handler.mt:_onActionId(action)
-	--log.df("unimplemented: handler:onActionId(actionFn)")
+    log.df("unimplemented: handler:onActionId(actionFn)")
 end
 
 --- plugins.core.action.handler:actionId(action) -> string
 --- Method
 --- Returns a string that can be used as a unique ID for the action details.
 function handler.mt:actionId(action)
-	return self._onActionId(action)
+    return self._onActionId(action)
 end
 
 -- plugins.core.action.handler._onExecute(action) -> nil
@@ -183,12 +200,12 @@ end
 -- Default handler for executing. Throws an error message.
 --
 -- Parameters:
--- * `action`	- The table of parameters being executed.
+-- * `action`   - The table of parameters being executed.
 --
 -- Returns:
 -- * Nothing
 function handler.mt._onExecute(action)
-	error("unimplemented: handler:onExecute(executeFn)")
+    log.df("unimplemented: handler:onExecute(executeFn)")
 end
 
 --- plugins.core.action.handler:execute(action) -> boolean
@@ -196,15 +213,15 @@ end
 --- Executes the action, based on values in the table.
 ---
 --- Parameters:
---- * `action`		- A table of details about the action.
+--- * `action`      - A table of details about the action.
 ---
 --- Returns:
 --- * `true` if the execution succeeded.
 function handler.mt:execute(action)
-	if action then
-		return self._onExecute(action) ~= false
-	end
-	return false
+    if action then
+        return self._onExecute(action) ~= false
+    end
+    return false
 end
 
 --- plugins.core.action.handler:reset([updateNow]) -> nil
@@ -212,15 +229,15 @@ end
 --- Resets the handler, clearing any cached result and requesting new ones.
 ---
 --- Parameters:
---- * `updateNow`	- (optional) If `true`, the choices will update immediately, otherwise they will update when the choices are next requested.
+--- * `updateNow`   - (optional) If `true`, the choices will update immediately, otherwise they will update when the choices are next requested.
 ---
 --- Returns:
 --- * Nothing
 function handler.mt:reset(updateNow)
-	self._choices = nil
-	if updateNow then
-		self.choices:update()
-	end
+    self._choices = nil
+    if updateNow then
+        self.choices:update()
+    end
 end
 
 return handler
