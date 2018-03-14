5e4bc305f57909969eab09b11d10582fd7f14183
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-03-15 08:36:25 +1100

    Fixed Typo in Remove Script

--- scripts/remove_commandpost.sh
+++ scripts/remove_commandpost.sh
@@ -9,7 +9,7 @@ rm ~/Library/Preferences/org.latenitefilms.CommandPost.plist
 
 echo "Trashing Application Support..."
 rm -R ~/Library/Application\ Support/CommandPost
-rm -R ~/Library/Application Support/org.latenitefilms.CommandPost
+rm -R ~/Library/Application\ Support/org.latenitefilms.CommandPost
 
 echo "Trashing Caches..."
 rm -R ~/Library/Preferences/Caches/org.latenitefilms.CommandPost
