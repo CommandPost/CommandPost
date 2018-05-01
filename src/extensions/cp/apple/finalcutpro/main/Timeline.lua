CommandPost/CommandPost

commit a925381bcda322f679c454742fd158bbc2305443

#1060 - Added checks for Viewer:isPlaying() to the scrolling/locked timeline (#1061)

src/extensions/cp/apple/finalcutpro/main/Timeline.lua
@@ -275,7 +275,16 @@ Timeline.isLockedPlayhead = prop.new(function(self)
 	return self._locked
 end):bind(Timeline)
 
--- TODO: Add documentation
+--- cp.apple.finalcut.main.Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre) -> self
+--- Method
+--- Locks the playhead on-screen.
+---
+--- Parameters:
+--- * deactivateWhenStopped	- If set to `true`, this will automatically deactivate itself when the playhead stops moving.
+--- * lockInCentre			- If set to `true`, the playhead will lock in the centre of the timeline. Otherwise, it will lock in it's current position.
+---
+--- Returns:
+--- * The `Timeline` instance.
 function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 	if self._locked then
 		-- already locked.
@@ -289,13 +298,18 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 	local lastPosition = nil
 	local stopCounter = 0
 	local originalOffset = 0
+	local viewer = self:app():viewer()
+	local threshold = Timeline.stopThreshold
 
 	local incPlayheadStopped = function()
-		stopCounter = math.min(Timeline.stopThreshold, stopCounter + 1)
+		stopCounter = math.min(threshold, stopCounter + 1)
 	end
 
 	local playheadHasStopped = function()
-		return stopCounter == Timeline.stopThreshold
+		if stopCounter ~= threshold and not viewer:isPlaying() then
+			stopCounter = threshold
+		end
+		return stopCounter == threshold
 	end
 
 	-- Setting this to false unlocks the playhead.
@@ -325,12 +339,12 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 			-- The timeline and/or playhead does not exist.
 			if status ~= Timeline.INVISIBLE then
 				status = Timeline.INVISIBLE
-				--log.df("Timeline not visible.")
+				-- log.df("Timeline not visible.")
 			end
 
-			stopCounter = Timeline.stopThreshold
+			stopCounter = threshold
 			if deactivateWhenStopped then
-				--log.df("Deactivating lock.")
+				-- log.df("Deactivating lock.")
 				self:unlockPlayhead()
 			end
 		else
@@ -343,7 +357,7 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 				incPlayheadStopped()
 				if playheadHasStopped() and status ~= Timeline.STOPPED then
 					status = Timeline.STOPPED
-					--log.df("Playhead stopped.")
+					-- log.df("Playhead stopped.")
 					if deactivateWhenStopped then
 						--log.df("Deactivating lock.")
 						self:unlockPlayhead()
@@ -362,12 +376,12 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 				if scrollTarget < 0 and scrollValue == 0 or scrollTarget > 1 and scrollValue == 1 then
 					if status ~= Timeline.DEADZONE then
 						status = Timeline.DEADZONE
-						--log.df("In the deadzone.")
+						-- log.df("In the deadzone.")
 					end
 				else
 					if status ~= Timeline.TRACKING then
 						status = Timeline.TRACKING
-						--log.df("Tracking the playhead.")
+						-- log.df("Tracking the playhead.")
 					end
 
 					-----------------------------------------------------------------------
@@ -395,13 +409,15 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
 		end
 	end
 
-	check()
+	-- Let's go!
+	timer.doAfter(Timeline.lockActive, check)
 
 	return self
 end
 
 -- TODO: Add documentation
 function Timeline:unlockPlayhead()
+	-- log.df("unlockPlayhead: called")
 	self._locked = false
 
 	return self

commit 23585c0e220f3e1f093859159cc51ab7659bf663

#1196
- Fixed issue with scrolling timeline, caused by changes made in #1061
- Closes #1196

src/extensions/cp/apple/finalcutpro/main/Timeline.lua
@@ -293,7 +293,7 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
     local lastPosition = nil
     local stopCounter = 0
     local originalOffset = 0
-    local viewer = self:app():viewer()
+    --local viewer = self:app():viewer()
     local threshold = Timeline.stopThreshold
 
     local incPlayheadStopped = function()
@@ -301,10 +301,13 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
     end
 
     local playheadHasStopped = function()
+        return stopCounter == threshold
+        --[[
         if stopCounter ~= threshold and not viewer:isPlaying() then
             stopCounter = threshold
         end
         return stopCounter == threshold
+        --]]
     end
 
     -- Setting this to false unlocks the playhead.
@@ -405,7 +408,8 @@ function Timeline:lockPlayhead(deactivateWhenStopped, lockInCentre)
     end
 
     -- Let's go!
-    timer.doAfter(Timeline.lockActive, check)
+    --timer.doAfter(Timeline.lockActive, check)
+    check()
 
     return self
 end