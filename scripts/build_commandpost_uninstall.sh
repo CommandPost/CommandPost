f73141bd977300cb4bc3587f2a1248960ca042c2
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-06-18 11:10:37 +1000

    Tweaked Uninstall Application
    - Uninstall script now actually removes the CommandPost application
    from the User Applications folder and/or the System Applications folder
    - Updated Uninstall script to use `killall` instead of AppleScript to
    force quit CommandPost to avoid potential hangs
    - Updated Uninstall built script to compile the AppleScript application
    and automatically replace the icon

--- scripts/build_commandpost_uninstall.sh
+++ scripts/build_commandpost_uninstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -rf scripts/inc/uninstall/Uninstall\ CommandPost.app
+osacompile -x -o scripts/inc/uninstall/Uninstall\ CommandPost.app scripts/inc/uninstall/Uninstall\ CommandPost.scpt
+cp scripts/inc/uninstall/applet.icns scripts/inc/uninstall/Uninstall\ CommandPost.app/Contents/Resources/applet.icns
 xattr -cr scripts/inc/uninstall/Uninstall\ CommandPost.app
 codesign --verbose --force --sign "Developer ID Application: LateNite Films Pty Ltd" scripts/inc/uninstall/Uninstall\ CommandPost.app
 codesign -dv --verbose=4 scripts/inc/uninstall/Uninstall\ CommandPost.app
