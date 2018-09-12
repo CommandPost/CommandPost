82e42547b4a5710e0b8175c94b9f9e9a4aea5558
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-09-12 15:35:16 +1000

    #1509
    - Fixed @stickler-ci errors

--- src/extensions/cp/apple/finalcutpro/main/TimelineToolbar.lua
+++ src/extensions/cp/apple/finalcutpro/main/TimelineToolbar.lua
@@ -25,7 +25,6 @@ local TimelineAppearance				= require("cp.apple.finalcutpro.main.TimelineAppeara
 --------------------------------------------------------------------------------
 local cache                             = axutils.cache
 local childFromLeft, childFromRight     = axutils.childFromLeft, axutils.childFromRight
-local childMatching, childWithID        = axutils.childMatching, axutils.childWithID
 local childWithRole                     = axutils.childWithRole
 
 --------------------------------------------------------------------------------
