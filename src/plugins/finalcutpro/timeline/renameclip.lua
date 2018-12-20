5a6190a799040c6cba4d202eb1e2ad431e1e00ac
Chris Hocking <latenitefilms@users.noreply.github.com>
2018-12-21 01:16:53 +1100

    #1583
    - Tweaks

--- src/plugins/finalcutpro/timeline/renameclip.lua
+++ src/plugins/finalcutpro/timeline/renameclip.lua
@@ -63,12 +63,12 @@ function plugin.init(deps)
                     local center = geoFrame and geoFrame.center
                     local topleft = geoFrame and geoFrame.topleft
                     local bottomright = geoFrame and geoFrame.bottomright
-                    local point = center and topleft and bottomright and geometry.point(topleft.x + 10, center.y)
+                    local point = center and topleft and bottomright and geometry.point(center.x, center.y)
                     if point and tools.isOffScreen(point) then
                         point = geometry.point(bottomright.x - 10, center.y)
                     end
                     if point and tools.isOffScreen(point) then
-                        point = geometry.point(center.x, center.y)
+                        point = geometry.point(topleft.x + 10, center.y)
                     end
                     if point and tools.isOffScreen(point) == false then
                         tools.ninjaRightMouseClick(point)
@@ -79,8 +79,12 @@ function plugin.init(deps)
                             item:performAction("AXPress")
                             return
                         else
-                            if menu then
-                                menu:performAction("AXShowMenu")
+                            local closeMenu = parent and axutils.childWithRole(parent, "AXMenu")
+                            if closeMenu then
+                                local closeMenuKids = closeMenu:attributeValue("AXChildren")
+                                if closeMenuKids and #closeMenuKids >= 1 then
+                                    closeMenuKids[1]:performAction("AXCancel")
+                                end
                             end
                         end
                     end
