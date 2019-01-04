b31b107c05d84bfafdaff680c33a77da725c7a14
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-01-05 00:08:48 +1100

    #1607
    - Fixed a bug that prevented the MIDI Learn function to work correctly.
    - If a row was previously highlighted red because of a duplicate entry,
    pressing the clear button will now remove the red highlight.
    - Closes #1607

--- src/plugins/core/midi/prefs/init.lua
+++ src/plugins/core/midi/prefs/init.lua
@@ -338,6 +338,16 @@ mod._midiCallbackInProgress = {}
 function mod._startLearning(id, params)
 
     --------------------------------------------------------------------------------
+    -- Save Group ID & Button ID both locally, and within the module, for the
+    -- callback:
+    --------------------------------------------------------------------------------
+    local groupID = params["groupID"]
+    local buttonID = params["buttonID"]
+
+    mod._learnGroupID = groupID
+    mod._learnButtonID = buttonID
+
+    --------------------------------------------------------------------------------
     -- Setup some locals:
     --------------------------------------------------------------------------------
     local injectScript = mod._manager.injectScript
@@ -361,27 +371,25 @@ function mod._startLearning(id, params)
     --------------------------------------------------------------------------------
     -- Start Learning Mode in JavaScript Land:
     --------------------------------------------------------------------------------
-    local groupID = params["groupID"]
-    local buttonID = params["buttonID"]
     injectScript("startLearnMode('" .. groupID .. "', '" .. buttonID .. "', '" .. i18n("stop") .. "')")
 
     --------------------------------------------------------------------------------
     -- Reset the current line item:
     --------------------------------------------------------------------------------
-    setValue(params["groupID"], params["buttonID"], "device", "")
-    setItem("device", params["buttonID"], params["groupID"], nil)
+    setValue(groupID, buttonID, "device", "")
+    setItem("device", buttonID, groupID, nil)
 
-    setValue(params["groupID"], params["buttonID"], "commandType", "")
-    setItem("commandType", params["buttonID"], params["groupID"], nil)
+    setValue(groupID, buttonID, "commandType", "")
+    setItem("commandType", buttonID, groupID, nil)
 
-    setValue(params["groupID"], params["buttonID"], "channel", "")
-    setItem("channel", params["buttonID"], params["groupID"], nil)
+    setValue(groupID, buttonID, "channel", "")
+    setItem("channel", buttonID, groupID, nil)
 
-    setValue(params["groupID"], params["buttonID"], "number", i18n("none"))
-    setItem("number", params["buttonID"], params["groupID"], nil)
+    setValue(groupID, buttonID, "number", i18n("none"))
+    setItem("number", buttonID, groupID, nil)
 
-    setValue(params["groupID"], params["buttonID"], "value", i18n("none"))
-    setItem("value", params["buttonID"], params["groupID"], nil)
+    setValue(groupID, buttonID, "value", i18n("none"))
+    setItem("value", buttonID, groupID, nil)
 
     --------------------------------------------------------------------------------
     -- Setup MIDI watchers:
@@ -401,6 +409,17 @@ function mod._startLearning(id, params)
         end
         if mod.learningMidiDevices[deviceName] then
             mod.learningMidiDevices[deviceName]:callback(function(_, callbackDeviceName, commandType, _, metadata)
+
+                local groupID = mod._learnGroupID
+                local buttonID = mod._learnButtonID
+
+                if not mod._currentlyLearning then
+                    --------------------------------------------------------------------------------
+                    -- First in, best dressed:
+                    --------------------------------------------------------------------------------
+                    return
+                end
+
                 if commandType == "controlChange" or commandType == "noteOn" or commandType == "pitchWheelChange" then
 
                     --------------------------------------------------------------------------------
@@ -408,6 +427,8 @@ function mod._startLearning(id, params)
                     --------------------------------------------------------------------------------
                     --log.df("commandType: %s", commandType)
                     --log.df("metadata: %s", hs.inspect(metadata))
+                    --log.df("groupID: %s", groupID)
+                    --log.df("buttonID: %s", buttonID)
 
                     --------------------------------------------------------------------------------
                     -- Support 14bit Control Change Messages:
@@ -463,25 +484,25 @@ function mod._startLearning(id, params)
                                 --------------------------------------------------------------------------------
                                 if deviceMatch and match then
 
-                                    log.wf("Duplicate MIDI Command Found")
+                                    --log.wf("Duplicate MIDI Command Found:\nGroup: %s\nButton: %s", groupID, buttonID)
 
                                     --------------------------------------------------------------------------------
                                     -- Reset the current line item:
                                     --------------------------------------------------------------------------------
-                                    setValue(params["groupID"], params["buttonID"], "device", "")
-                                    setItem("device", params["buttonID"], params["groupID"], nil)
+                                    setValue(groupID, buttonID, "device", "")
+                                    setItem("device", buttonID, groupID, nil)
 
-                                    setValue(params["groupID"], params["buttonID"], "commandType", "")
-                                    setItem("commandType", params["buttonID"], params["groupID"], nil)
+                                    setValue(groupID, buttonID, "commandType", "")
+                                    setItem("commandType", buttonID, groupID, nil)
 
-                                    setValue(params["groupID"], params["buttonID"], "channel", "")
-                                    setItem("channel", params["buttonID"], params["groupID"], nil)
+                                    setValue(groupID, buttonID, "channel", "")
+                                    setItem("channel", buttonID, groupID, nil)
 
-                                    setValue(params["groupID"], params["buttonID"], "number", i18n("none"))
-                                    setItem("number", params["buttonID"], params["groupID"], nil)
+                                    setValue(groupID, buttonID, "number", i18n("none"))
+                                    setItem("number", buttonID, groupID, nil)
 
-                                    setValue(params["groupID"], params["buttonID"], "value", i18n("none"))
-                                    setItem("value", params["buttonID"], params["groupID"], nil)
+                                    setValue(groupID, buttonID, "value", i18n("none"))
+                                    setItem("value", buttonID, groupID, nil)
 
                                     --------------------------------------------------------------------------------
                                     -- Exit the callback:
@@ -502,42 +523,39 @@ function mod._startLearning(id, params)
                     -- Update the UI & Save Preferences:
                     --------------------------------------------------------------------------------
                     if metadata.isVirtual then
-                        setValue(params["groupID"], params["buttonID"], "device", "virtual_" .. callbackDeviceName)
-                        setItem("device", params["buttonID"], params["groupID"], "virtual_" .. callbackDeviceName)
+                        setValue(groupID, buttonID, "device", "virtual_" .. callbackDeviceName)
+                        setItem("device", buttonID, groupID, "virtual_" .. callbackDeviceName)
                     else
-                        setValue(params["groupID"], params["buttonID"], "device", callbackDeviceName)
-                        setItem("device", params["buttonID"], params["groupID"], callbackDeviceName)
+                        setValue(groupID, buttonID, "device", callbackDeviceName)
+                        setItem("device", buttonID, groupID, callbackDeviceName)
                     end
 
-                    setValue(params["groupID"], params["buttonID"], "commandType", commandType)
-                    setItem("commandType", params["buttonID"], params["groupID"], commandType)
+                    setValue(groupID, buttonID, "commandType", commandType)
+                    setItem("commandType", buttonID, groupID, commandType)
 
-                    setValue(params["groupID"], params["buttonID"], "channel", metadata.channel)
-                    setItem("channel", params["buttonID"], params["groupID"], metadata.channel)
+                    setValue(groupID, buttonID, "channel", metadata.channel)
+                    setItem("channel", buttonID, groupID, metadata.channel)
 
                     if commandType == "noteOff" or commandType == "noteOn" then
 
-                        setValue(params["groupID"], params["buttonID"], "number", metadata.note)
-                        setItem("number", params["buttonID"], params["groupID"], metadata.note)
+                        setValue(groupID, buttonID, "number", metadata.note)
+                        setItem("number", buttonID, groupID, metadata.note)
 
-                        setValue(params["groupID"], params["buttonID"], "value", i18n("none"))
-                        setItem("value", params["buttonID"], params["groupID"], i18n("none"))
+                        setValue(groupID, buttonID, "value", i18n("none"))
+                        setItem("value", buttonID, groupID, i18n("none"))
 
                     elseif commandType == "controlChange" then
 
-                        setValue(params["groupID"], params["buttonID"], "number", metadata.controllerNumber)
-                        setItem("number", params["buttonID"], params["groupID"], metadata.controllerNumber)
+                        setValue(groupID, buttonID, "number", metadata.controllerNumber)
+                        setItem("number", buttonID, groupID, metadata.controllerNumber)
 
-                        setValue(params["groupID"], params["buttonID"], "value", controllerValue)
-                        setItem("value", params["buttonID"], params["groupID"], controllerValue)
+                        setValue(groupID, buttonID, "value", controllerValue)
+                        setItem("value", buttonID, groupID, controllerValue)
 
                     elseif commandType == "pitchWheelChange" then
 
-                        --setValue(params["groupID"], params["buttonID"], "number", "Pitch")
-                        --setItem("number", params["buttonID"], params["groupID"], "Pitch")
-
-                        setValue(params["groupID"], params["buttonID"], "value", metadata.pitchChange)
-                        setItem("value", params["buttonID"], params["groupID"], metadata.pitchChange)
+                        setValue(groupID, buttonID, "value", metadata.pitchChange)
+                        setItem("value", buttonID, groupID, metadata.pitchChange)
 
                     end
 
@@ -565,6 +583,7 @@ end
 -- Returns:
 --  * None
 local function midiPanelCallback(id, params)
+    local injectScript = mod._manager.injectScript
     local callbackType = params and params["type"]
     if callbackType then
         if callbackType == "updateAction" then
@@ -642,6 +661,11 @@ local function midiPanelCallback(id, params)
 
             setValue(params["groupID"], params["buttonID"], "value", i18n("none"))
             mod._midi.setItem("value", params["buttonID"], params["groupID"], nil)
+
+            --------------------------------------------------------------------------------
+            -- Remove the red highlight if it's still there:
+            --------------------------------------------------------------------------------
+            injectScript("unhighlightRowRed('" .. params["groupID"] .. "', " .. params["buttonID"] .. ")")
         elseif callbackType == "updateNumber" then
             --------------------------------------------------------------------------------
             -- Update Number:
