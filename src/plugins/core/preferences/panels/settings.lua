2edaff56884455ed26dc2f9a3d7d78cec545581c
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-04-06 00:09:58 +1100

    Added Temporary Settings Preferences Panel
    - Added a temporary settings preferences panel so users can easily
    access the settings folder. Will replace with something better at some
    point.

---
+++ src/plugins/core/preferences/panels/settings.lua
@@ -0,0 +1,52 @@
+--- === plugins.core.preferences.panels.settings ===
+---
+--- Settings Preferences Panel
+
+local require       = require
+
+local hs            = hs
+
+local image         = require("hs.image")
+
+local i18n          = require("cp.i18n")
+
+local execute       = hs.execute
+local imageFromName = image.imageFromName
+
+--------------------------------------------------------------------------------
+--
+-- THE PLUGIN:
+--
+--------------------------------------------------------------------------------
+local plugin = {
+    id              = "core.preferences.panels.settings",
+    group           = "core",
+    dependencies    = {
+        ["core.preferences.manager"]    = "manager",
+    }
+}
+
+function plugin.init(deps)
+    local panel = deps.manager.addPanel({
+        priority    = 9000,
+        id          = "settings",
+        label       = i18n("settings"),
+        image       = imageFromName("NSAdvanced"),
+        tooltip     = i18n("settings"),
+        height      = 170,
+    })
+
+    panel
+        :addContent(1, [[<p style="padding-left:20px;">In a future release of CommandPost we will have a proper import/export feature for settings. In the meantime...</p>]], false)
+        :addButton(2,
+            {
+                label 	    = "Open User Settings Folder",
+                width       = 200,
+                onclick	    = function() execute([[open ~/Library/Application\ Support/CommandPost]]) end,
+            }
+        )
+
+    return panel
+end
+
+return plugin
