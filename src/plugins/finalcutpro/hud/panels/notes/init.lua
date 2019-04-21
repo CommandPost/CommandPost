f8c72ca5857e92ba41efcd8332fdeb388741a8cd
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-04-21 20:03:32 +1000

    #1818
    - Fixed @stickler-ci bug

--- src/plugins/finalcutpro/hud/panels/notes/init.lua
+++ src/plugins/finalcutpro/hud/panels/notes/init.lua
@@ -4,7 +4,7 @@
 
 local require                   = require
 
-local log                       = require "hs.logger".new "notes"
+--local log                       = require "hs.logger".new "notes"
 
 local fs                        = require "hs.fs"
 local image                     = require "hs.image"
