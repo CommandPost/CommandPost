969334f830aa95a00eed676ec4cdee5854d3438d
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-06-07 02:32:22 +1000

    #1875
    - Improved the performance of the MIDI Manager by using tables instead
    of a loop of doom.
    - Removed unused MIDI commands from the UI.
    - Updated the timeline zoom MIDI control.

--- src/plugins/finalcutpro/midi/controls/zoom.lua
+++ src/plugins/finalcutpro/midi/controls/zoom.lua
@@ -4,66 +4,11 @@
 
 local require = require
 
-local log               = require("hs.logger").new("zoomMIDI")
+local log               = require "hs.logger".new "zoomMIDI"
 
-local fcp               = require("cp.apple.finalcutpro")
-local i18n              = require("cp.i18n")
-
-
-local mod = {}
-
---- plugins.finalcutpro.midi.controls.zoom.control() -> nil
---- Function
---- Final Cut Pro MIDI Zoom Control
----
---- Parameters:
----  * metadata - table of metadata from the MIDI callback
----
---- Returns:
----  * None
-function mod.control(metadata)
-    local value
-    if metadata.pitchChange then
-        value = metadata.pitchChange
-    else
-        value = metadata.fourteenBitValue
-    end
-    if type(value) == "number" then
-        local appearance = fcp:timeline():toolbar():appearance()
-        if appearance then
-            --------------------------------------------------------------------------------
-            -- MIDI Controller Value (7bit):        0 to 127
-            -- MIDI Controller Value (14bit):       0 to 16383
-            -- Zoom Slider:                         0 to 10
-            --------------------------------------------------------------------------------
-            appearance:show():zoomAmount():setValue(value / (16383/10))
-        end
-    else
-        log.ef("Unexpected Value: %s %s", value, value and type(value))
-    end
-end
-
---- plugins.finalcutpro.midi.controls.zoom.init() -> nil
---- Function
---- Initialise the module.
----
---- Parameters:
----  * None
----
---- Returns:
----  * None
-function mod.init()
-    local params = {
-        group = "fcpx",
-        text = string.upper(i18n("midi")) .. ": " .. i18n("timelineZoom"),
-        subText = i18n("midiTimelineZoomDescription"),
-        fn = mod.control,
-    }
-    mod._manager.controls:new("zoomSlider", params)
-
-    return mod
-end
-
+local deferred          = require "cp.deferred"
+local fcp               = require "cp.apple.finalcutpro"
+local i18n              = require "cp.i18n"
 
 local plugin = {
     id              = "finalcutpro.midi.controls.zoom",
@@ -74,8 +19,34 @@ local plugin = {
 }
 
 function plugin.init(deps)
-    mod._manager = deps.manager
-    return mod.init()
+    local manager = deps.manager
+
+    local cachedValue
+
+    local updateUI = deferred.new(0.01):action(function()
+        if cachedValue then
+            fcp:timeline():toolbar():appearance():show():zoomAmount():setValue(cachedValue / (16383/10))
+        end
+    end)
+
+    local control = function(metadata)
+        local value
+        if metadata.pitchChange then
+            value = metadata.pitchChange
+        else
+            value = metadata.fourteenBitValue
+        end
+        cachedValue = value
+        updateUI()
+    end
+
+    local params = {
+        group = "fcpx",
+        text = string.upper(i18n("midi")) .. ": " .. i18n("timelineZoom") .. " (" .. i18n("absolute") .. ")",
+        subText = i18n("midiTimelineZoomDescription"),
+        fn = control,
+    }
+    manager.controls:new("zoomSlider", params)
 end
 
 return plugin
