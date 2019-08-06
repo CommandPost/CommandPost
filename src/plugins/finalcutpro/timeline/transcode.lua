7b349f19d18b87e60926ee4056df7e28a07445ba
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-08-06 21:52:02 +1000

    Cleanup
    - Cleaned up code and added some more checks

--- src/plugins/finalcutpro/timeline/transcode.lua
+++ src/plugins/finalcutpro/timeline/transcode.lua
@@ -11,6 +11,7 @@ local i18n              = require "cp.i18n"
 local just              = require "cp.just"
 local tools             = require "cp.tools"
 
+local doUntil           = just.doUntil
 local playErrorSound    = tools.playErrorSound
 
 local plugin = {
@@ -22,46 +23,70 @@ local plugin = {
 }
 
 function plugin.init(deps)
+
+    local contents = fcp:timeline():contents()
+    local transcodeMedia = fcp:transcodeMedia()
+
     deps.fcpxCmds
         :add("createOptimizedMediaFromTimeline")
         :titled(i18n("createOptimizedMediaOfSelectedClipsInTimeline"))
         :whenActivated(function()
-            local contents = fcp:timeline():contents()
+            if not fcp:launch() then
+                playErrorSound()
+                return
+            end
+
+            if not doUntil(function()
+                fcp:selectMenu({"Window", "Go To", "Timeline"})
+                return contents:isFocused()
+            end) then
+                playErrorSound()
+                return
+            end
+
             local selectedClipsUI = contents:selectedClipsUI()
             if not selectedClipsUI then
                 playErrorSound()
                 return
             end
             for _, clip in pairs(selectedClipsUI) do
-                contents:selectClip(clip)
+                if not doUntil(function()
+                    contents:selectClip(clip)
+                    local selectedClips = contents:selectedClipsUI()
+                    return selectedClips and #selectedClips == 1 and selectedClips[1] == clip
+                end) then
+                    playErrorSound()
+                    return
+                end
+
                 fcp:selectMenu({"File", "Reveal in Browser"})
                 fcp:selectMenu({"Window", "Go To", "Libraries"})
 
-                if not just.doUntil(function()
+                if not doUntil(function()
                     return fcp:menu():isEnabled({"File", "Transcode Media…"})
                 end) then
                     playErrorSound()
                     return
                 end
                 fcp:selectMenu({"File", "Transcode Media…"})
-                if not just.doUntil(function()
-                    return fcp:transcodeMedia():isShowing()
+                if not doUntil(function()
+                    return transcodeMedia:isShowing()
                 end) then
                     playErrorSound()
                     return
                 end
 
-                if not fcp:transcodeMedia():createOptimizedMedia():isEnabled() then
-                    fcp:transcodeMedia():cancel():press()
+                if not transcodeMedia:createOptimizedMedia():isEnabled() then
+                    transcodeMedia:cancel():press()
                     playErrorSound()
                     return
                 end
 
-                fcp:transcodeMedia():createOptimizedMedia():checked(true)
-                fcp:transcodeMedia():ok():press()
+                transcodeMedia:createOptimizedMedia():checked(true)
+                transcodeMedia:ok():press()
 
-                if not just.doUntil(function()
-                    return not fcp:transcodeMedia():isShowing()
+                if not doUntil(function()
+                    return not transcodeMedia:isShowing()
                 end) then
                     playErrorSound()
                     return
@@ -73,42 +98,62 @@ function plugin.init(deps)
         :add("createProxyMediaFromTimeline")
         :titled(i18n("createProxyMediaOfSelectedClipsInTimeline"))
         :whenActivated(function()
-            local contents = fcp:timeline():contents()
+            if not fcp:launch() then
+                playErrorSound()
+                return
+            end
+
+            if not doUntil(function()
+                fcp:selectMenu({"Window", "Go To", "Timeline"})
+                return contents:isFocused()
+            end) then
+                playErrorSound()
+                return
+            end
+
             local selectedClipsUI = contents:selectedClipsUI()
             if not selectedClipsUI then
                 playErrorSound()
                 return
             end
             for _, clip in pairs(selectedClipsUI) do
-                contents:selectClip(clip)
+                if not doUntil(function()
+                    contents:selectClip(clip)
+                    local selectedClips = contents:selectedClipsUI()
+                    return selectedClips and #selectedClips == 1 and selectedClips[1] == clip
+                end) then
+                    playErrorSound()
+                    return
+                end
+
                 fcp:selectMenu({"File", "Reveal in Browser"})
                 fcp:selectMenu({"Window", "Go To", "Libraries"})
 
-                if not just.doUntil(function()
+                if not doUntil(function()
                     return fcp:menu():isEnabled({"File", "Transcode Media…"})
                 end) then
                     playErrorSound()
                     return
                 end
                 fcp:selectMenu({"File", "Transcode Media…"})
-                if not just.doUntil(function()
-                    return fcp:transcodeMedia():isShowing()
+                if not doUntil(function()
+                    return transcodeMedia:isShowing()
                 end) then
                     playErrorSound()
                     return
                 end
 
-                if not fcp:transcodeMedia():createProxyMedia():isEnabled() then
-                    fcp:transcodeMedia():cancel():press()
+                if not transcodeMedia:createProxyMedia():isEnabled() then
+                    transcodeMedia:cancel():press()
                     playErrorSound()
                     return
                 end
 
-                fcp:transcodeMedia():createProxyMedia():checked(true)
-                fcp:transcodeMedia():ok():press()
+                transcodeMedia:createProxyMedia():checked(true)
+                transcodeMedia:ok():press()
 
-                if not just.doUntil(function()
-                    return not fcp:transcodeMedia():isShowing()
+                if not doUntil(function()
+                    return not transcodeMedia:isShowing()
                 end) then
                     playErrorSound()
                     return
