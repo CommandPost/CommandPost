29d0f3740cba1b34a39b07fd84c152b7333c906d
David Peterson <david@randombits.org>
2019-09-30 02:02:02 +1000

    * Added extra test cases.

--- src/tests/cp/interpolate_spec.lua
+++ src/tests/cp/interpolate_spec.lua
@@ -21,4 +21,14 @@ return describe "cp.interpolate" {
         expect(interpolate("Hello ${world:04d}", { world = 512 })):is("Hello 0512")
     end),
 
+    it "uses % operator"
+    :doing(function()
+        expect(("Hello ${world}") % { world = "Mars" }):is("Hello Mars")
+    end),
+
+    it "replaces multiple tokens"
+    :doing(function()
+        expect(("${greeting} ${world}") % { greeting = "Goodbye", world = "Pluto"}):is("Goodbye Pluto")
+    end)
+
 }
