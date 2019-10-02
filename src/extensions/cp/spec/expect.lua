d1c0b3276b9b542993cd0141f663028917ea2d34
David Peterson <david@randombits.org>
2019-10-02 18:45:50 +1000

    * Added 'deepeq' to expect for comparing tables.

--- src/extensions/cp/spec/expect.lua
+++ src/extensions/cp/spec/expect.lua
@@ -16,16 +16,56 @@ local format    = string.format
 
 local Expect = class("cp.spec.Expect")
 
+local function deepeq(a, b)
+    -- Different types: false
+    if type(a) ~= type(b) then return false end
+    -- Functions
+    if type(a) == 'function' then
+        return string.dump(a) == string.dump(b)
+    end
+    -- Primitives and equal pointers
+    if a == b then return true end
+    -- Only equal tables could have passed previous tests
+    if type(a) ~= 'table' then return false end
+    -- Compare tables field by field
+    for k,v in pairs(a) do
+        if b[k] == nil or not deepeq(v, b[k]) then
+            -- check for special `n` key for table length
+            if k ~= "n" or b.n ~= nil or a.n ~= #b then
+                return false
+            end
+        end
+    end
+    for k,v in pairs(b) do
+        if a[k] == nil or not deepeq(v, a[k]) then
+            -- check for special `n` key for table length
+            if k ~= "n" or a.n ~= nil or b.n ~= #a then
+                return false
+            end
+        end
+    end
+    return true
+end
+
+local function deepneq(a, b)
+    local ok, _ = deepeq(a, b)
+    if ok then
+        return false
+    else
+        return true
+    end
+end
+
 function Expect:initialize(value)
     self.value = value
 end
 
 function Expect:is(other)
-    assert(self.value == other, format("Expected value of %s, but it was %s", inspect(other), inspect(self.value)), 2)
+    assert(deepeq(self.value, other), format("Expected value to be %s, but it was %s", inspect(other), inspect(self.value)), 2)
 end
 
 function Expect:isNot(other)
-    assert(self.value ~= other, format("Expected value to not be %s, but it was %s", inspect(other), inspect(self.value)), 2)
+    assert(deepneq(self.value, other), format("Expected value to not be %s, but it was %s", inspect(other), inspect(self.value)), 2)
 end
 
 function Expect:isAtLeast(other)
