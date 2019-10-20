f39a4c012bf0356ac6c790be2c62f0abc38f0e39
Chris Hocking <latenitefilms@users.noreply.github.com>
2019-10-20 19:03:28 +1100

    Unified the Global Console and FCPX Console
    - Separated AppleScript from the Applications action handler.
    - You can now access Final Cut Pro actions from the formerly Global
    Console. This means you can now just have some system-wide shortcut to
    access the Search Console.
    - Added a Search Console icon for “Applications”.
    - Closes #908

--- src/plugins/finalcutpro/console/console.lua
+++ src/plugins/finalcutpro/console/console.lua
@@ -5,7 +5,10 @@
 local require   = require
 
 local config    = require "cp.config"
+local fcp       = require "cp.apple.finalcutpro"
 local tools     = require "cp.tools"
+
+local mod = {}
 
 local plugin = {
     id              = "finalcutpro.console",
@@ -13,60 +16,62 @@ local plugin = {
     dependencies    = {
         ["finalcutpro.commands"]        = "fcpxCmds",
         ["core.action.manager"]         = "actionmanager",
+        ["core.console"]                = "console",
     }
 }
 
 function plugin.init(deps)
     --------------------------------------------------------------------------------
-    -- Initialise Module:
+    -- Register a Final Cut Pro specific activator for the Search Console:
     --------------------------------------------------------------------------------
-    local activator
-    local cmds = deps.fcpxCmds
-    local actionmanager = deps.actionmanager
+    local bundleID = fcp:bundleID()
+    deps.console.register(bundleID, function()
+        if not mod.activator then
+            local actionmanager = deps.actionmanager
+            mod.activator = actionmanager.getActivator("finalcutpro.console"):preloadChoices()
+
+            --------------------------------------------------------------------------------
+            -- Don't show widgets in the Final Cut Pro Search Console:
+            --------------------------------------------------------------------------------
+            local allowedHandlers = {}
+            local handlerIds = actionmanager.handlerIds()
+            for _,id in pairs(handlerIds) do
+                local handlerTable = tools.split(id, "_")
+                if handlerTable[2]~= "widgets" then
+                    table.insert(allowedHandlers, id)
+                end
+            end
+            mod.activator:allowHandlers(table.unpack(allowedHandlers))
+
+            --------------------------------------------------------------------------------
+            -- Allow specific toolbar icons in the Console:
+            --------------------------------------------------------------------------------
+            local coreIconPath = config.basePath .. "/plugins/core/console/images/"
+            local iconPath = config.basePath .. "/plugins/finalcutpro/console/images/"
+            local toolbarIcons = {
+                global_applications = { path = coreIconPath .. "apps.png",      priority = 1},
+                fcpx_videoEffect    = { path = iconPath .. "videoEffect.png",   priority = 2},
+                fcpx_audioEffect    = { path = iconPath .. "audioEffect.png",   priority = 3},
+                fcpx_generator      = { path = iconPath .. "generator.png",     priority = 4},
+                fcpx_title          = { path = iconPath .. "title.png",         priority = 5},
+                fcpx_transition     = { path = iconPath .. "transition.png",    priority = 6},
+                fcpx_fonts          = { path = iconPath .. "font.png",          priority = 7},
+                fcpx_shortcuts      = { path = iconPath .. "shortcut.png",      priority = 8},
+                fcpx_menu           = { path = iconPath .. "menu.png",          priority = 9},
+            }
+            mod.activator:toolbarIcons(toolbarIcons)
+        end
+        return mod.activator
+    end)
 
     --------------------------------------------------------------------------------
-    -- Add the command trigger:
+    -- Add a Final Cut Pro specific action to open the Search Console:
     --------------------------------------------------------------------------------
+    local cmds = deps.fcpxCmds
     cmds:add("cpConsole")
         :groupedBy("commandPost")
         :whenActivated(function()
-            if not activator then
-                activator = actionmanager.getActivator("finalcutpro.console")
-                    :preloadChoices()
-
-                --------------------------------------------------------------------------------
-                -- Don't show widgets in the console:
-                --------------------------------------------------------------------------------
-                local allowedHandlers = {}
-                local handlerIds = actionmanager.handlerIds()
-                for _,id in pairs(handlerIds) do
-                    local handlerTable = tools.split(id, "_")
-                    if handlerTable[2]~= "widgets" then
-                        table.insert(allowedHandlers, id)
-                    end
-                end
-                activator:allowHandlers(table.unpack(allowedHandlers))
-
-                --------------------------------------------------------------------------------
-                -- Allow specific toolbar icons in the Console:
-                --------------------------------------------------------------------------------
-                local iconPath = config.basePath .. "/plugins/finalcutpro/console/images/"
-                local toolbarIcons = {
-                    fcpx_videoEffect    = { path = iconPath .. "videoEffect.png",   priority = 1},
-                    fcpx_audioEffect    = { path = iconPath .. "audioEffect.png",   priority = 2},
-                    fcpx_generator      = { path = iconPath .. "generator.png",     priority = 3},
-                    fcpx_title          = { path = iconPath .. "title.png",         priority = 4},
-                    fcpx_transition     = { path = iconPath .. "transition.png",    priority = 5},
-                    fcpx_fonts          = { path = iconPath .. "font.png",          priority = 6},
-                    fcpx_shortcuts      = { path = iconPath .. "shortcut.png",      priority = 7},
-                    fcpx_menu           = { path = iconPath .. "menu.png",          priority = 8},
-                }
-                activator:toolbarIcons(toolbarIcons)
-
-            end
-
-            activator:toggle()
-
+            deps.console.show()
         end)
         :activatedBy():ctrl("space")
 
