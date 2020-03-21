85ff840b6d836241db57e6e28dcaf59b5d194323
Chris Hocking <latenitefilms@users.noreply.github.com>
2020-03-21 19:31:57 +1100

    Updated hs._asm.axuielement to 0.7.3-coroutineFriendly

--- src/extensions/hs/_asm/axuielement/init.lua
+++ src/extensions/hs/_asm/axuielement/init.lua
@@ -1,3 +1,7 @@
+-- maybe save some pain, if the shim is installed; otherwise, expect an objc dump to console when this loads on stock Hammerspoon without pull #2308 applied
+if package.searchpath("hs._asm.coroutineshim", package.path) then
+    require"hs._asm.coroutineshim"
+end
 
 --- === hs._asm.axuielement ===
 ---
