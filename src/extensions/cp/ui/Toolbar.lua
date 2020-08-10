7c44b4d1e3a1816c04a768e93ffe448e5ee86405
David Peterson <david@randombits.org>
2020-08-11 05:44:14 +1000

    Code Cleanup

--- src/extensions/cp/ui/Toolbar.lua
+++ src/extensions/cp/ui/Toolbar.lua
@@ -4,11 +4,11 @@
 
 local require                       = require
 
-local axutils						= require("cp.ui.axutils")
-local Button                        = require("cp.ui.Button")
-local Do                            = require("cp.rx.go").Do
-local Element                       = require("cp.ui.Element")
-local prop							= require("cp.prop")
+local axutils						= require "cp.ui.axutils"
+local Button                        = require "cp.ui.Button"
+local Do                            = require "cp.rx.go.Do"
+local Element                       = require "cp.ui.Element"
+local prop							= require "cp.prop"
 
 
 local Toolbar = Element:subclass("cp.ui.Toolbar")
