f817194b89d7dda0859a77bcfd6472deb9793d4d
Chris Hocking <latenitefilms@users.noreply.github.com>
2021-04-09 01:52:57 +1000

    Reworked the FCPX commands plugin
    - Closes #2652

--- src/plugins/finalcutpro/commands/commands.lua
+++ src/plugins/finalcutpro/commands/commands.lua
@@ -7,6 +7,9 @@ local require       = require
 
 --local log           = require "hs.logger".new "fcpCmds"
 
+local application   = require "hs.application"
+local appWatcher    = require "hs.application.watcher"
+
 local commands      = require "cp.commands"
 local fcp           = require "cp.apple.finalcutpro"
 
@@ -24,33 +27,35 @@ function plugin.init()
     if not fcp:isSupported() then return end
 
     --------------------------------------------------------------------------------
-    -- New Final Cut Pro Command Collection:
+    -- Initialise the Final Cut Pro Commands:
     --------------------------------------------------------------------------------
     mod.cmds = commands.new("fcpx")
 
-    --------------------------------------------------------------------------------
-    -- Switch to Final Cut Pro to activate:
-    --------------------------------------------------------------------------------
-    mod.cmds:watch({
-        activate    = function()
-            fcp:launch()
-        end,
-    })
+    local checkFrontmostApp = function()
+        local frontmostApplication = application.frontmostApplication()
+        local bundleID = frontmostApplication:bundleID()
+        if bundleID and bundleID == fcp:bundleID() then
+            mod.cmds:isEnabled(true)
+        else
+            mod.cmds:isEnabled(false)
+        end
+    end
 
     --------------------------------------------------------------------------------
-    -- Enable/Disable as Final Cut Pro becomes Active/Inactive:
+    -- Watch for Final Cut Pro becoming active:
     --------------------------------------------------------------------------------
-    mod.isEnabled = fcp.isFrontmost:AND(fcp.isModalDialogOpen:NOT()):watch(function(enabled)
-        mod.cmds:isEnabled(enabled)
-    end):label("fcpxCommandsIsEnabled")
+    mod._appWatcher = appWatcher.new(function(_, event)
+        if event == appWatcher.activated then
+            checkFrontmostApp()
+        end
+    end):start()
+
+    --------------------------------------------------------------------------------
+    -- Check on CommandPost's launch:
+    --------------------------------------------------------------------------------
+    checkFrontmostApp()
 
     return mod.cmds
-end
-
-function plugin.postInit()
-    if mod.isEnabled then
-        mod.isEnabled:update()
-    end
 end
 
 return plugin
